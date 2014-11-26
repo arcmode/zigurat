487ce8aec33e75e3831b3da8106a9b830254c250
David Rojas <drojascamaggi@gmail.com>
2014-11-25 23:56:15 -0300

    refactor

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojure-opennlp "0.3.2"]]
-  :profiles {:dev {:dependencies []}}
-  :test-refresh {:notify-command ["notice"]
-                 :notify-on-success true})
+                 [clojure-opennlp     "0.3.2"]
+                 [inflections         "0.9.7"]]
+  :profiles {:dev {:dependencies []
+                   :plugins [[com.jakemccrary/lein-test-refresh "0.5.2"]]}}
+  :test-refresh {:notify-on-success true})
--- src/zigurat/core.clj
+++ src/zigurat/core.clj
@@ -6,64 +6,81 @@
 
   {:doc/format :markdown}
 
-  (:use opennlp.nlp)
-  (:use opennlp.treebank)
-  (:use clojure.pprint))
+  (:require [opennlp.nlp      :refer [make-sentence-detector
+                                      make-tokenizer
+                                      make-pos-tagger]]
+            [opennlp.treebank :refer [make-treebank-chunker]]
+            [inflections.core :refer :all]
+            [clojure.string   :refer (join)]))
 
-;; Opennlp primitives.
+;;
+;; An atom to store the mapping from nlp domain to our data model
+;; It is `schema` the correct word?
+;;
+(def ^:dynamic *schema* (atom (read-string (slurp "schema/default-en.edn"))))
+
+
+(let [test-str "school"
+      test-reg (re-pattern (str "(?i)" (singular test-str) "|" (plural test-str)))]
+  (boolean (re-matches test-reg "Schools")))
+
+
+;; OpenNLP tools
 
 (def ^:private get-sentences (make-sentence-detector "models/en-sent.bin"))
 (def ^:private tokenize (make-tokenizer "models/en-token.bin"))
 (def ^:private pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
 (def ^:private chunker (make-treebank-chunker "models/en-chunker.bin"))
 
+
 ;; tagging
 
-(defn- tag-sent-seq
-  "post-tag a sequence of sentences"
-  [sent-seq]
-  (map #(-> % tokenize pos-tag chunker) sent-seq))
+(defn sentences
+  {:doc "Applies `tokenize` and `postag` to each sentence returned by `get-sentences`"
+   :test (fn []
+           (assert (= '((["[last"   "JJ"]
+                         ["words"   "NNS"]
+                         [","       ","]
+                         ["turning" "VBG"]
+                         ["on"      "IN"]
+                         ["Clu"     "NNP"]
+                         ["during"  "IN"]
+                         ["an"      "DT"]
+                         ["air"     "NN"]
+                         ["battle"  "NN"]
+                         ["]"       "IN"]
+                         ["I"       "PRP"]
+                         ["fight"   "VBP"]
+                         ["for"     "IN"]
+                         ["the"     "DT"]
+                         ["Users"   "NNS"]
+                         ["!"       "."]))
+                      (sentences "[last words, turning on Clu during an air battle]
+                                 I fight for the Users!"))))
+   }
+  [text]
+  (let [reader-func (comp (partial map (comp pos-tag
+                                             tokenize))
+                          get-sentences)]
+    (reader-func text)))
 
-;; cypher
 
-(defn- cypher-phrase
-  "find phrases in the cypher bank"
-  [{phrase :phrase}]
-  phrase)
+(defn sym-list-to-string
+  {:doc "Returns the joined string representation of the symbols in sym-list"
+   :test (fn []
+           (assert (= "will not eval"
+                      (sym-list-to-string '(will not eval)))))}
+  [sym-list]
+  (join " " (map str sym-list)))
 
-(defn- cypher-sent
-  "Cypher a sentence.
 
-  1. Find defined phrases
-  "
-  [sent]
-  (map cypher-phrase sent))
+(defmacro read-raw
+  {:doc "Applies form-to-strings and ~method to the ~payload."
+   :test (fn []
+           (assert (= '(clojure.core/-> (quote (will not eval))
+                                        zigurat.core/sym-list-to-string
+                                        identity)
+                       (macroexpand-1 '(read-raw identity will not eval)))))}
+  [method & payload]
+  `(-> '~payload sym-list-to-string ~method))
 
-(defn- cypher-sent-seq
-  "cypher a sequence of sentences"
-  [sent-seq]
-  (map #(-> % tokenize pos-tag chunker cypher-sent) sent-seq))
-
-;;
-;; public
-;;
-
-(defn tag-sentences
-  "Return pos-tagged chunks from a natural language string with sentences"
-  [nl-form]
-  (-> nl-form get-sentences tag-sent-seq))
-
-;;
-;; TODO: reuse tag-sentences
-;;
-(defn cypher-sentences
-  "Convert a natural language string to cypher:
-
-  1. Pos-tag a natural language form.
-  2. Searches for cypher entities.
-  3. Guess an optimum representation picking one entity by tag."
-
-  {:doc/format :markdown}
-
-  [nl-form]
-  (-> nl-form get-sentences cypher-sent-seq))
